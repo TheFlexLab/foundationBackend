71683694d617eabc427a7e2edf7e2ffde7baa6c4
mmahad911 <mmahad913@gmail.com>
2024-05-28 18:55:09 +0500

    Hashed Data.
    Hashed Data.

--- utils/security.js
+++ utils/security.js
@@ -25,14 +25,14 @@ const decryptData = (data) => {
 };
 
 const userCustomizedEncryptData = (data, eyk) => {
-    const cipher = crypto.createCipheriv(ALGORITHM, crypto.createHash('sha256').update(eyk).digest(), new Buffer('1234567812345678', 'binary'));
+    const cipher = crypto.createCipheriv(ALGORITHM, eyk, new Buffer('1234567812345678', 'binary'));
     let encrypted = cipher.update(data instanceof Buffer ? data : JSON.stringify(data), 'utf8', 'hex');
     encrypted += cipher.final('hex');
     return encrypted;
 };
 
 const userCustomizedDecryptData = (data, eyk) => {
-    const decipher = crypto.createDecipheriv(ALGORITHM, crypto.createHash('sha256').update(eyk).digest(), new Buffer('1234567812345678', 'binary'));
+    const decipher = crypto.createDecipheriv(ALGORITHM, eyk, new Buffer('1234567812345678', 'binary'));
     let decrypted = decipher.update(data, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
     try {
