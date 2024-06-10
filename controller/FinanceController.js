36be2082926509553f9bb1ad930422417872daa6
mmahad911 <mmahad913@gmail.com>
2024-06-10 14:18:03 +0500

    Not in 100s
    Not in 100s

--- controller/FinanceController.js
+++ controller/FinanceController.js
@@ -161,7 +161,7 @@ const getStripePaymentIntent = async (req, res) => {
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: amount * 100,
+    amount: amount,
     currency: currency,
     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
     automatic_payment_methods: {
