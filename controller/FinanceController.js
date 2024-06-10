bb1861665101affb052753ddc25784120d194fdf
mmahad911 <mmahad913@gmail.com>
2024-06-10 17:44:58 +0500

    Stripe Intent only for Card.
    Stripe Intent only for Card.

--- controller/FinanceController.js
+++ controller/FinanceController.js
@@ -164,6 +164,7 @@ const getStripePaymentIntent = async (req, res) => {
   const paymentIntent = await stripe.paymentIntents.create({
     amount: amount * 100,
     currency: currency,
+    payment_method_types: ['card'],
     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
     automatic_payment_methods: {
       enabled: true,
