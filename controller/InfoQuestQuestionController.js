38e741a41bb6dca58ae8c9916e217f5983410e06
mmahad911 <mmahad913@gmail.com>
2024-07-19 16:04:48 +0500

    Home Feed Notifications Setting.
    Home Feed Notifications Setting.

--- controller/InfoQuestQuestionController.js
+++ controller/InfoQuestQuestionController.js
@@ -1447,7 +1447,10 @@ const getQuestsAll = async (req, res) => {
             notification5,
             notification6,
             notification7,
-            notification8;
+            notification8,
+            notification9,
+            notification10,
+            notification11;
 
           if (mode && page === 1) {
             // Define Guest's notification1 properties
@@ -1487,129 +1490,166 @@ const getQuestsAll = async (req, res) => {
           } else if (!mode) {
             notification1 = {
               id: "system_notification",
-              icon: "https://www.flickr.com/photos/160246067@N08/39735543880/",
-              header: "How it works",
+              header: "Build your data portfolio",
               text: [
-                "On Foundation, you can post content, participate in posts and earn badges. These activities add value to your data, making you more desirable for companies looking to advertise to people like you.",
+                "By adding data to your Foundation profile, you are building a valuable asset. What is adding data? Simply creating posts, participating in posts and adding badges. The more valuable your data assets, the more opportunities you'll have to earn from sources who wish to pay you for it.",
               ],
               buttonText: "",
               buttonUrl: "",
-              category: "Home",
-              position: "Feed",
+              youtubeEmbedUrl: "",
+              category: "homeFeeds",
+              mode: "User, Guest",
               priority: 1,
-              mode: "User",
               timestamp: new Date().toISOString(),
             };
 
             notification2 = {
               id: "system_notification",
-              icon: "https://www.flickr.com/photos/160246067@N08/39735543880/",
-              header: "How to use Foundation",
+              header: "Anonymity is key",
               text: [
-                "Start by participating in posts and adding badges to your profile. Each interaction enhances the value of your data and the network, increasing your earning potential.",
+                "All data you generate by participating, creating and adding vericiationbadges on Foundation is anonymous. There is no way for others to link your activity to your identity. Your data is encrypted, even from us!",
               ],
               buttonText: "",
               buttonUrl: "",
-              category: "Home",
-              position: "Feed",
+              youtubeEmbedUrl: "",
+              category: "homeFeeds",
+              mode: "User, Guest",
               priority: 4,
-              mode: "User",
               timestamp: new Date().toISOString(),
             };
 
             notification3 = {
               id: "system_notification",
-              icon: "https://www.flickr.com/photos/160246067@N08/39735543880/",
-              header: "Engage with posts, earn FDX",
+              header: "How to use Foundation",
               text: [
-                "Start participating in posts to earn FDX tokens. Keep scrolling or sort by category up top to participate in posts you care about.",
+                "Start by participating in posts, creating posts and adding verification badges to your profile. Each interaction enhances the value of your data and your potential to earn.",
               ],
-              buttonText: "",
-              buttonUrl: "",
-              category: "Home",
-              position: "Feed",
+              buttonText: "Learn more",
+              buttonUrl: "https://on.foundation/help/about",
+              youtubeEmbedUrl: "",
+              category: "homeFeeds",
+              mode: "User, Guest",
               priority: 8,
-              mode: "User",
               timestamp: new Date().toISOString(),
             };
 
             notification4 = {
               id: "system_notification",
-              icon: "https://www.flickr.com/photos/160246067@N08/39735543880/",
-              header: "What is FDX?",
+              header: "Engage with posts, earn FDX",
               text: [
-                "FDX (Foundation Data Exchange Tokens) represent the value of data on the Foundation network. Every interaction on Foundation earns FDX, which can be used on the platform or sold for dollars once enough data demand is established.",
+                "Earn FDX tokens for every post participation. Keep scrolling and see how many posts you can participate in, or filter based on what you like!",
               ],
-              buttonText: "My FDX",
-              buttonUrl: "/treasury",
-              category: "Home",
-              position: "Feed",
+              buttonText: "",
+              buttonUrl: "",
+              youtubeEmbedUrl: "",
+              category: "homeFeeds",
+              mode: "User, Guest",
               priority: 12,
-              mode: "User",
               timestamp: new Date().toISOString(),
             };
 
             notification5 = {
               id: "system_notification",
-              icon: "https://www.flickr.com/photos/160246067@N08/39735543880/",
-              header: "Want to create your own post?",
+              header: "What is FDX?",
               text: [
-                "It costs FDX tokens to create a post but every time someone else engages with it, you earn FDX tokens. If 1 million people engage with your post, you will have a lot of tokens!",
+                "FDX (Foundation Data Exchange) tokens represent the value of data on the Foundation network. Every interaction on Foundation earns FDX and increases the value of the network.",
               ],
-              buttonText: "Create a Post",
-              buttonUrl: "/post",
-              category: "Home",
-              position: "Feed",
+              buttonText: "My FDX",
+              buttonUrl: "https://on.foundation/treasury",
+              youtubeEmbedUrl: "",
+              category: "homeFeeds",
+              mode: "User, Guest",
               priority: 16,
-              mode: "User",
               timestamp: new Date().toISOString(),
             };
 
             notification6 = {
               id: "system_notification",
-              icon: "https://www.flickr.com/photos/160246067@N08/39735543880/",
-              header: "Anonymity is key",
+              header: "Integrity matters",
               text: [
-                "Share your views, insights and data anonymously on Foundation. You control who can link your data to you, ensuring both privacy and proper compensation.",
+                "Foundation values integrity and transparency. If users attempt to manipulate the system by adding misleading options or unfairly objecting to good options, they risk damaging their reputation. This can lead to reduced visibility of their posts and lower token earnings.",
               ],
               buttonText: "",
               buttonUrl: "",
-              category: "Home",
-              position: "Feed",
+              youtubeEmbedUrl: "",
+              category: "homeFeeds",
+              mode: "User, Guest",
               priority: 20,
-              mode: "User",
               timestamp: new Date().toISOString(),
             };
 
             notification7 = {
               id: "system_notification",
-              icon: "https://www.flickr.com/photos/160246067@N08/39735543880/",
-              header: "Build your data portfolio",
+              header: "What does Foundation do with my data I've added? ",
               text: [
-                "By consistently adding data to your Foundation profile, you're creating a valuable asset. Companies are willing to pay for authentic data with no middle man, and you earn tokens for your contributions.",
+                "Nothing. Foundation does not own the information you populate within the platform. You own your data and can decide what sources you want to share it with and what price. ",
               ],
-              buttonText: "",
-              buttonUrl: "",
-              category: "Home",
-              position: "Feed",
+              buttonText: "Learn more",
+              buttonUrl: "https://on.foundation/help/faq",
+              youtubeEmbedUrl: "",
+              category: "homeFeeds",
+              mode: "User, Guest",
               priority: 24,
-              mode: "User",
               timestamp: new Date().toISOString(),
             };
 
             notification8 = {
               id: "system_notification",
-              icon: "https://www.flickr.com/photos/160246067@N08/39735543880/",
-              header: "Integrity matters",
+              header: "Want to create your own post?",
               text: [
-                "Foundation values integrity and transparency. If users attempt to manipulate the system by adding misleading options or unfairly objecting to good answers, they risk damaging their reputation. This can lead to reduced visibility of their posts and answers, as well as lower earnings of tokens.",
+                "It costs FDX tokens to create a post but every time someone else engages with it, you earn more FDX tokens. If 1 million people engage with your post, you will have a lot of tokens!",
+              ],
+              buttonText: "Create a Post",
+              buttonUrl: "https://on.foundation/post",
+              youtubeEmbedUrl: "",
+              category: "homeFeeds",
+              mode: "User, Guest",
+              priority: 28,
+              timestamp: new Date().toISOString(),
+            };
+
+            notification9 = {
+              id: "system_notification",
+              header: "Looking for something specific?",
+              text: [
+                "User our search bar to find anything you like. Choose different feed types and categories from our easy to use slider! Utilize our powerful Filters to find posts by type, participation, media, rating and more!",
               ],
               buttonText: "",
               buttonUrl: "",
-              category: "Home",
-              position: "Feed",
-              priority: 28,
-              mode: "User",
+              youtubeEmbedUrl: "",
+              category: "homeFeeds",
+              mode: "User, Guest",
+              priority: 32,
+              timestamp: new Date().toISOString(),
+            };
+
+            notification10 = {
+              id: "system_notification",
+              header: "Share your posts!",
+              text: [
+                "Sharing posts is a great way earn FDX. Even if it is not your post, you will earn FDX anytime someone participates through your unique shared links! Only you can see the results from the audience you have shared with!",
+              ],
+              buttonText: "",
+              buttonUrl: "",
+              youtubeEmbedUrl: "",
+              category: "homeFeeds",
+              mode: "User, Guest",
+              priority: 36,
+              timestamp: new Date().toISOString(),
+            };
+
+            notification11 = {
+              id: "system_notification",
+              header: "What are lists?",
+              text: [
+                "You can build your own custom lists to be shared with anyone you like. They can be used like surverys or questionaires, or ways to group relevent posts. They can even be used like media play lists! You also get to see the results from just the audience you have shared your list with!",
+              ],
+              buttonText: "",
+              buttonUrl: "",
+              youtubeEmbedUrl: "",
+              category: "homeFeeds",
+              mode: "User, Guest",
+              priority: 40,
               timestamp: new Date().toISOString(),
             };
             // Page 1
