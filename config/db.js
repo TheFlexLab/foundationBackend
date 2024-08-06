94d2f2a211af531ecd7afd13720047fa1f3a98bf
mmahad911 <mmahad913@gmail.com>
2024-08-06 22:11:55 +0500

    DOc DB reverted
    DOc DB reverted

--- config/db.js
+++ config/db.js
@@ -2,21 +2,21 @@ const mongoose = require("mongoose");
 const DocumentDB = process.env.DATABASE;
 
 const connectDB = async () => {
-  if (true) {
+  if (DocumentDB === "DocumentDB") {
     try {
       const conn = await mongoose.connect(
         "mongodb://" +
-          "foundationDatabse" +
+          process.env.DOCS_DB_USER +
           ":" +
-          "foundation" +
-          "@foundation.cluster-chkicsa8wkr6.us-east-2.docdb.amazonaws.com:27017/" + "foundationDevelopmentDocDb" +"?tls=true&tlsCAFile=global-bundle.pem" +
+          process.env.DOCS_DB_PASSWORD +
+          "@foundation.cluster-chkicsa8wkr6.us-east-2.docdb.amazonaws.com:27017/" + process.env.DOCS_DB_NAME +"?tls=true&tlsCAFile=global-bundle.pem" +
           "&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
       );
       console.log(
         `DocumentDB Connected! Database Name: ${conn.connection.db.databaseName} -  Host: ${conn.connection.host}`.cyan.underline.bold
       );
     } catch (err) {
-      console.log("Could not establish connection to DocDB! " + err.message);
+      //console.log("Could not establish connection to DocDB! " + err);
     }
   } else {
     try {
@@ -35,5 +35,40 @@ const connectDB = async () => {
     }
   }
 };
+
+// const connectDB = async () => {
+//   if (true) {
+//     try {
+//       const conn = await mongoose.connect(
+//         "mongodb://" +
+//           "foundationDatabse" +
+//           ":" +
+//           "foundation" +
+//           "@foundation.cluster-chkicsa8wkr6.us-east-2.docdb.amazonaws.com:27017/" + "foundationDevelopmentDocDb" +"?tls=true&tlsCAFile=global-bundle.pem" +
+//           "&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+//       );
+//       console.log(
+//         `DocumentDB Connected! Database Name: ${conn.connection.db.databaseName} -  Host: ${conn.connection.host}`.cyan.underline.bold
+//       );
+//     } catch (err) {
+//       console.log("Could not establish connection to DocDB! " + err.message);
+//     }
+//   } else {
+//     try {
+//       const conn = await mongoose.connect(process.env.MONGO_URI, {
+//         user: process.env.MONGO_USER,
+//         pass: process.env.MONGO_PASSWORD,
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       });
+
+//       console.log(
+//         `MongoDB Connected! Database Name: ${conn.connection.db.databaseName} -  Host: ${conn.connection.host}`.cyan.underline.bold
+//       );
+//     } catch (err) {
+//       //console.log("Could not establish connection to MongoDB! " + err);
+//     }
+//   }
+// };
 
 module.exports = connectDB;
